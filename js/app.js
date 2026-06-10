d52708fe2585f7b23b2ddfca6c7d354e9564a4cd
Frenchitas <134218245+Frenchitas@users.noreply.github.com>
2026-06-10 09:32:54 -0300

    feat: add playlist game

--- js/app.js
+++ js/app.js
@@ -1,20 +1,59 @@
 console.log("Mini Games iniciado");
 
+// ====================
+// JUEGOS
+// ====================
+
+// Juego 1: Piedra Papel Tijera
 function jugarPiedraPapelTijera() {
     alert("Entraste a Piedra, Papel o Tijera");
 }
 
+// Juego 2: Cara o Cruz
 function jugarCaraOCruz() {
     alert("Entraste a Cara o Cruz");
 }
 
+// Juego 3: Adivina el Número
 function jugarAdivinaNumero() {
     alert("Entraste a Adivina  el Número");
 }
 
+// Juego 4: Playlist
 function crearPlaylist() {
     alert("Entraste a Crear una Playlist");
+
+    const playlist = prompt("Ingrese el nombre de la playlist");
+
+let cantidad = Number(prompt("¿Cuántas canciones desea agregar?"));
+
+const canciones = [];
+
+for (let i = 0; i < cantidad; i++) {
+
+    const cancion = prompt("Ingresa una canción:");
+
+    canciones.push(cancion);
+
+    const faltan = cantidad - (i + 1);
+
+    if (faltan > 0) {
+        alert(`Te quedan ${faltan} canciones por agregar`);
+    } else {
+        alert("Ya agregaste todas las canciones");
+    }
+
+    alert(`Playlist actual: ${canciones}`);
 }
+
+alert(`Tu playlist es: ${playlist}
+con las siguientes canciones:
+${canciones.join(", ")}`);
+}
+
+// ====================
+// MENÚ PRINCIPAL
+// ====================
 
 let opcion = "";
 
